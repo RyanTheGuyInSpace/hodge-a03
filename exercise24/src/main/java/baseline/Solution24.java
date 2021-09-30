5675d8d75b06ab0dc811c188a82add9aa02b6067
RyanTheGuyInSpace <ryo_hodge@hotmail.com>
2021-09-30 12:33:02 -0400

    Added pseudocode and skeleton for exercise 24

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,30 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Ryan Hodge
+ */
+
+package baseline;
+
+public class Solution24 {
+    public static void main(String[] args) {
+
+        // Print a message that explains to the user that we're going to detect anagrams
+        // Prompt the user for two strings and store them
+        // Compare the strings to determine whether they are anagrams of each other
+        // Print whether the two strings are anagrams or not
+
+
+    }
+
+    /** Compares two string to determine whether they are anagrams of each other
+     *
+     * @param a First string to compare
+     * @param b Second string to compare
+     * @return True if the two strings are anagrams, false otherwise
+     */
+    public boolean isAnagram(String a, String b) {
+
+
+        return false;
+    }
+}
