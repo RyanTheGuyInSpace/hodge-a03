a9da8eb40ae4f957d6d1ca37633447f50dcc14e8
RyanTheGuyInSpace <ryo_hodge@hotmail.com>
2021-10-02 19:54:07 -0400

    Added pseudocode and skeleton for exercise26

---
+++ exercise26/src/main/java/baseline/PaymentCalculator.java
@@ -0,0 +1,30 @@
+package baseline;
+
+public class PaymentCalculator {
+
+    /**
+     * The amount of money to start from.
+     */
+    int startingBalance;
+
+    /**
+     * How much interest the starting balance accrues, expressed as a percent.
+     */
+    int interestRate;
+
+    /**
+     * The amount that will be paid toward the balance monthly.
+     */
+    int monthlyPaymentAmount;
+
+    /**
+     * Calculates how many months it will take to pay off a credit card with the given starting balance,
+     * interest rate and monthly payment amount.
+     *
+     * @return The number of months it will take to pay off the card.
+     */
+    public int calculateMonthsToPayOff() {
+
+        return -1;
+    }
+}
---
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -0,0 +1,17 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Ryan Hodge
+ */
+
+package baseline;
+
+public class Solution26 {
+    public static void main(String[] args) {
+
+        // Prompt the user for their starting balance
+        // Prompt the user for the APR on the card as a percent
+        // Prompt the user for the monthly payment they're capable of making
+        // Calculate how many months it will take to pay off the card
+        // and print it back to the user
+    }
+}
