337759af4d8648d79cd39f9023ee8f1d1750b524
RyanTheGuyInSpace <ryo_hodge@hotmail.com>
2021-10-03 19:24:27 -0400

    Added pseudocode for solution 32

---
+++ exercise31/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+T
+$src/main/java/baseline/Karvonen.java,a\d\adbc3089db62473646aba009328e153d6e701429
+X
+(src/test/java/baseline/KarvonenTest.java,3\3\335667eb2ee315dea8ea91ac5f6a6296dab066b2
+V
+&src/main/java/baseline/Solution31.java,5\c\5cafb99e82926136c2f1d044c79185ad9c24e74e
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Ryan Hodge
+ */
+
+package baseline;
+
+public class Solution32 {
+    public static void main(String[] args) {
+        // Prompt the user to select difficulty 1, 2 or 3
+        // Generate a random number for each difficulty.
+        // Difficulty 1 = 1 - 10
+        // Difficulty 2 = 1 - 100
+        // Difficulty 3 = 1 - 1000
+        // Loop while asking the user for a guess at the number
+        // Each incorrect guess increments a guess counter
+        // Each incorrect guess print a hint to user, indicating whether the guess
+        // was too high or too low
+        // When the user guesses the number correctly, print a success message and the number of
+        // incorrect guesses that were made
+
+
+    }
+}
