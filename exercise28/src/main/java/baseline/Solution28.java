d900e0e06c73538432952eaef82dae31ef40e51e
RyanTheGuyInSpace <ryo_hodge@hotmail.com>
2021-10-03 13:55:31 -0400

    Added pseudocode for solution 28

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,15 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Ryan Hodge
+ */
+
+package baseline;
+
+public class Solution28 {
+    public static void main(String[] args) {
+        // Loop 5 times to ask a user for a number
+        // Each iteration of the loop, add the input to an integer outside of the loop
+        // The integer outside of the loop must be initialized at 0
+        // Print out the sum of the 5 numbers to the user
+    }
+}
