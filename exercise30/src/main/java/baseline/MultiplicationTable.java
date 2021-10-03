bb4593ac0755fb33b8602d82a0e794dfef27c67d
RyanTheGuyInSpace <ryo_hodge@hotmail.com>
2021-10-03 14:48:09 -0400

    Added pseudocode and skeleton for solution 30

---
+++ exercise30/src/main/java/baseline/MultiplicationTable.java
@@ -0,0 +1,11 @@
+package baseline;
+
+public class MultiplicationTable {
+    /**
+     * Prints out multiples 1 - 12 for the given number
+     * @param n The number whose multiples will be printed from 1 - 12
+     */
+    public static void printLine(int n) {
+
+    }
+}
---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,12 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Ryan Hodge
+ */
+
+package baseline;
+
+public class Solution30 {
+    public static void main(String[] args) {
+        // Use a nested loop to generate each line of the multiplication table up to 12
+    }
+}
