d949d13284d72a897944b0a2ec20c5f345afe7da
RyanTheGuyInSpace <ryo_hodge@hotmail.com>
2021-10-03 20:19:16 -0400

    Added pseudocode for exercise 34

---
+++ exercise34/src/main/java/baseline/Employees.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Ryan Hodge
+ */
+
+package baseline;
+
+public class Employees {
+    public String[] employees = {
+            "John Smith",
+            "Jackie Jackson",
+            "Chris Jones",
+            "Amanda Cullen",
+            "Jeremy Goodwin"
+    };
+
+    /**
+     * Prints all the employees inside of the employees array
+     */
+    public void printEmployees() {
+
+    }
+
+    /**
+     * Removes the employee with the given name from the employees array.
+     * @param employeeName The name of the employee to remove from the employees array.
+     */
+    public void removeEmployee(String employeeName) {
+
+    }
+}
---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,15 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Ryan Hodge
+ */
+
+package baseline;
+
+public class Solution34 {
+    public static void main(String[] args) {
+        // Print out all the employees inside the list
+        // Prompt the user to type the name of the employee to remove from the list
+        // After removal, print the new list of employees
+
+    }
+}
