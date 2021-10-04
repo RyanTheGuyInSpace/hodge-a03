10939c56a7e627138681f920ac5fe30ddad81458
RyanTheGuyInSpace <ryo_hodge@hotmail.com>
2021-10-03 20:05:47 -0400

    Added pseudocode for exercise 33

---
+++ exercise33/src/main/java/baseline/Magic8Ball.java
@@ -0,0 +1,15 @@
+package baseline;
+
+public class Magic8Ball {
+
+    private String[] MagicResponses = { "Yes.", "No.", "Maybe.", "Ask again later." };
+
+    /**
+     * Gets a response from the Magic8Ball
+     * @param n The number to use to get a response from the Magic8Ball
+     * @return A response from the Magic8Ball
+     */
+    public static String getResponse(int n) {
+        return "";
+    }
+}
---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,10 @@
+package baseline;
+
+public class Solution33 {
+    public static void main(String[] args) {
+        // Prompt the user to ask a question
+        // When the user asks a question, generate a random number between
+        // 1 and 4 and feed it into the Magic8Ball
+        // Print the response
+    }
+}
